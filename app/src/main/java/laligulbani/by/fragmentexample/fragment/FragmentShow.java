57e6c248bd6a78004269aadfc563394daca1d737
Lali Gulbani <lalushka9711@gmail.com>
2017-10-27 20:54:58 +0300

    1button, show , hide

---
+++ app/src/main/java/laligulbani/by/fragmentexample/fragment/FragmentShow.java
@@ -0,0 +1,29 @@
+package laligulbani.by.fragmentexample.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import laligulbani.by.fragmentexample.R;
+
+
+public class FragmentShow extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment1, container, false);
+        Button show = (Button)view.findViewById(R.id.button_show);
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    return view;
+    }
+
+}
